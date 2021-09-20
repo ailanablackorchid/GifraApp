c6fc05066fc2c450c2dcf772d26f58c0cfef4ce4
ailanablackorchid <ailana.askar@nu.edu.kz>
2021-09-21 05:10:08 +0600

    FlatList implementation

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, TextInput, View, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  FlatList,
+} from "react-native";
 import { GiphyFetch } from "@giphy/js-fetch-api";
 
 export default function App() {
   // search query
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
   const giphy = new GiphyFetch("1MgWwcx6vB72YYFMZw0zSTscgiW7fLk2");
 
   const fetchGifs = async () => {
-    const res = await giphy.search(query, { limit: 5 });
+    const res = await giphy.search(query);
     setResults(res.data);
   };
 
   useEffect(() => {
+    // try {
+    //   setIsLoading(true);
+    //   setTimeout(() => {
+    //   fetchGifs();
+    // }, 1000);
+    // setIsLoading(false);
     fetchGifs();
+    // } catch (e) {
+    //   console.log(e.message);
+    // } finally {
+    // }
   }, [query]);
+
+  const renderItem = ({ item }) => (
+    <img
+      key={item.id}
+      src={item.images.fixed_height_downsampled.url}
+      width={200}
+      height={200}
+    />
+  );
+
+  const onRefresh = () => {
+    setIsRefreshing(true);
+    fetchGifs();
+    setIsRefreshing(false);
+  };
 
   return (
     <View style={styles.container}>
@@ -27,23 +61,13 @@ export default function App() {
         placeholder="Search GIPHY"
         onChange={(e) => setQuery(e.target.value)}
       />
-      <ScrollView>{}</ScrollView>
-
-      {results
-        ? results.map((item) => {
-            console.log(item.embed_url);
-            return (
-              <img
-                key={item.id}
-                src={item.images.fixed_height_downsampled.url}
-                width={200}
-                height={200}
-              />
-            );
-          })
-        : null}
-
-      <StatusBar style="auto" />
+      <FlatList
+        data={results}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        refreshing={isRefreshing}
+        onRefresh={onRefresh}
+      />
     </View>
   );
 }
