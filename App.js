1c65c10893da37a8c6868a0ecedcbe21fa7de778
ailanablackorchid <ailana.askar@nu.edu.kz>
2021-09-21 03:13:46 +0600

    project basic initialization - api and view set up

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, TextInput, View, ScrollView } from "react-native";
+import { GiphyFetch } from "@giphy/js-fetch-api";
 
 export default function App() {
+  // search query
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  const giphy = new GiphyFetch("1MgWwcx6vB72YYFMZw0zSTscgiW7fLk2");
+
+  const fetchGifs = async () => {
+    const res = await giphy.search(query, { limit: 5 });
+    setResults(res.data);
+    console.log(results);
+  };
+
+  useEffect(() => {
+    fetchGifs();
+  }, [query]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        value={query}
+        style={styles.input}
+        placeholder="Search GIPHY"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Text>{query}</Text>
+
+      <ScrollView>{}</ScrollView>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +40,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  input: {
+    padding: "5px 15px",
+    margin: "5px 0",
+    borderRadius: 3,
+    borderWidth: 1,
   },
 });
